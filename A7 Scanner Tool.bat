8f6da20af449f1515deac4d33da6a9f3be5fda20
a7ecc <102755240+a7ecc@users.noreply.github.com>
2023-09-11 21:35:36 +0400

    Update A7 Scanner Tool.bat

--- A7 Scanner Tool.bat
+++ A7 Scanner Tool.bat
@@ -16,35 +16,39 @@ echo  Start scanning all devices connected to the network at %CurrentTime%
 echo.
 echo.
 for /f "tokens=2,3 delims={,}" %%a in ('"WMIC NICConfig where IPEnabled="True" get DefaultIPGateway /value | find "I" "') do if not defined ip set ip=%%~a
+set ip3=%ip:~0,-1%
 nmap -sn %ip%/24 | findstr /v /l "Host is up" | findstr /v /l "addresses" | findstr /v /l "Starting" > "%temp%\Network Devices"
 :restart1
 type "%temp%\Network Devices"
 echo.
 echo.
 :ReEnterTargetIP
-set /p TargetIP=Type the IP address of the target device:
+set /p TargetIP=Type the last three digits of the target's IP address:
 if "%TargetIP%"=="" goto ReEnterTargetIP
+if defined TargetIP if "%TargetIP:~3,1%"=="" (goto true) else (goto ReEnterTargetIP)
+:true
 cls
 echo  +---------------------------------------------------------------------+
 echo  +                           A7 Scanner Tool                           +
 echo  +---------------------------------------------------------------------+
 echo              Start scanning the IP address at %CurrentTime%
-echo                         IP Address : %ip%.%TargetIP%
+echo                         IP Address : %ip3%%TargetIP%
 echo.
 echo.
 if not exist "A7 Scanner Tool Results" md "A7 Scanner Tool Results"
-nmap -sV "%ip%.%TargetIP%"> "A7 Scanner Tool Results\%ip%.%TargetIP%.txt"
-type "A7 Scanner Tool Results\%ip%.%TargetIP%.txt"
+nmap -sV "%ip3%%TargetIP%"> "A7 Scanner Tool Results\%ip3%%TargetIP%.txt"
+type "A7 Scanner Tool Results\%ip3%%TargetIP%.txt"
 echo ----End of the scan----
 echo.
 echo.
 echo If you find any vulnerability, list it here, and if you don't find a vulnerability, press the ENTER button directly
 echo.
 :research
+set search=
 set /p search=Type the name of the vulnerability:
 if "%search%"=="" goto restart
 set search=%search: =+%
-start https://www.rapid7.com/db/?q=%search%^&type=nexpose
+start https://www.google.com/search?q=%search%+vulnerabilities
 goto research
 :restart
 cls
