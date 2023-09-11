de89aecb3f516de2b33c1d61e60d2f15d80b8807
a7ecc <102755240+a7ecc@users.noreply.github.com>
2023-09-11 20:09:51 +0400

    Update A7 Scanner Tool.bat

--- A7 Scanner Tool.bat
+++ A7 Scanner Tool.bat
@@ -6,9 +6,9 @@ echo  +---------------------------------------------------------------------+
 if not exist "%systemdrive%\Program Files (x86)\Nmap" echo You must download NMAP software to use this tool
 if not exist "%systemdrive%\Program Files (x86)\Nmap" %windir%\System32\choice.exe /m "Do You Want to download NMAP software?"
 if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 1 start https://nmap.org/dist/nmap-7.94-setup.exe
-if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 1 echo ---End of Tool---
+if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 1 echo ---End of A7 Scanner Tool---
 if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 1 pause > nul & exit
-if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 2 echo ---End of Tool---
+if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 2 echo ---End of A7 Scanner Tool---
 if not exist "%systemdrive%\Program Files (x86)\Nmap" IF %ERRORLEVEL% EQU 2 pause > nul & exit
 for /f "tokens=2,3,4,5,6 usebackq delims=:/ " %%a in ('%date% %time%') do set CurrentTime=%%b-%%a-%%c %%d:%%e
 set scanninglog=Start scanning all devices connected to the network at %CurrentTime%
@@ -21,7 +21,6 @@ nmap -sn %ip%/24 | findstr /v /l "Host is up" | findstr /v /l "addresses" | find
 type "%temp%\Network Devices"
 echo.
 echo.
-echo.
 :ReEnterTargetIP
 set /p TargetIP=Type the IP address of the target device:
 if "%TargetIP%"=="" goto ReEnterTargetIP
@@ -30,24 +29,23 @@ echo  +---------------------------------------------------------------------+
 echo  +                           A7 Scanner Tool                           +
 echo  +---------------------------------------------------------------------+
 echo              Start scanning the IP address at %CurrentTime%
-echo                         IP Address : %TargetIP%
+echo                         IP Address : %ip%.%TargetIP%
 echo.
 echo.
 if not exist "A7 Scanner Tool Results" md "A7 Scanner Tool Results"
-nmap -sV "%TargetIP%"> "A7 Scanner Tool Results\%TargetIP%.txt"
-type "A7 Scanner Tool Results\%TargetIP%.txt"
-echo.
+nmap -sV "%ip%.%TargetIP%"> "A7 Scanner Tool Results\%ip%.%TargetIP%.txt"
+type "A7 Scanner Tool Results\%ip%.%TargetIP%.txt"
 echo ----End of the scan----
 echo.
-echo If you find any vulnerability, list it here, and if you don't find a vulnerability, press the ENTER button directly
-set /p search=Type the name of the vulnerability:
-set search=%search: =+%
-start www.google.com/search?q=%search%+vulnerabilities+site:rapid7.com
 echo.
-echo ---End of Tool---
-:loop
-pause > nul
-goto loop
+echo If you find any vulnerability, list it here, and if you don't find a vulnerability, press the ENTER button directly
+echo.
+:research
+set /p search=Type the name of the vulnerability:
+if "%search%"=="" goto restart
+set search=%search: =+%
+start https://www.rapid7.com/db/?q=%search%^&type=nexpose
+goto research
 :restart
 cls
 echo  +---------------------------------------------------------------------+
